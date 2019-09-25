4d279d61557336bd9484219907b690b1beae480b
realchenglong <scl2012@msn.cn>
2019-09-25 19:38:19 +0800

    博客文章的管理2019年9月25日19:38:11

--- src/main/java/controller/BlogController.java
+++ src/main/java/controller/BlogController.java
@@ -1,12 +1,16 @@
 package controller;
 
 import model.BlogEntity;
+import model.UserEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import repository.BlogRepository;
+import repository.UserRepository;
 
 import java.util.List;
 
@@ -14,7 +18,8 @@ import java.util.List;
 public class BlogController {
     @Autowired
     BlogRepository blogRepository;
-
+    @Autowired
+    UserRepository userRepository;
     // 查看所有博文
     @RequestMapping(value = "/admin/blogs",method = RequestMethod.GET)
     public String showBlogs(ModelMap modelMap){
@@ -22,4 +27,51 @@ public class BlogController {
         modelMap.addAttribute("blogList",blogEntityList);
         return "admin/blogs";
     }
+
+    @RequestMapping(value = "/admin/blogs/add",method = RequestMethod.GET)
+    public String addBlog(ModelMap modelMap){
+        List<UserEntity> userEntities = userRepository.findAll();
+        modelMap.addAttribute("userList",userEntities);
+        return "admin/addBlog";
+    }
+
+    @RequestMapping(value = "/admin/blogs/addPost",method = RequestMethod.POST)
+    public String addBlogPost(@ModelAttribute("blog") BlogEntity blogEntity){
+        System.out.println(blogEntity.getTitle());
+        System.out.println(blogEntity.getUserByUserId().getFirstName());
+        blogRepository.saveAndFlush(blogEntity);
+        return "redirect:/admin/blogs";
+    }
+
+    @RequestMapping(value = "admin/blogs/show/{id}")
+    public String showBlog(@PathVariable("id") int id,ModelMap modelMap){
+        BlogEntity blog = blogRepository.findById(id).get();
+        modelMap.addAttribute("blog",blog);
+        return "admin/blogDetail";
+    }
+
+    @RequestMapping(value = "/admin/blogs/update/{id}")
+    public String updateBlog(@PathVariable("id") int id,ModelMap modelMap){
+        BlogEntity blogEntity = blogRepository.findById(id).get();
+        List<UserEntity> userEntities = userRepository.findAll();
+        modelMap.addAttribute("blog",blogEntity);
+        modelMap.addAttribute("userList",userEntities);
+        return  "admin/updateBlog";
+    }
+
+    @RequestMapping(value = "admin/blogs/updateP",method = RequestMethod.POST)
+    public String updateBlogPost(@ModelAttribute("blogP") BlogEntity blogEntity){
+        blogRepository.updateBlogs(blogEntity.getTitle(),blogEntity.getUserByUserId().getId(),blogEntity.getContent()
+                ,blogEntity.getPubDate(),blogEntity.getId());
+        blogRepository.flush();
+        return "redirect:/admin/blogs";
+    }
+
+    @RequestMapping(value = "admin/blogs/delete/{id}",method = RequestMethod.GET)
+    public String deleteBlog(@PathVariable("id") int id){
+        blogRepository.deleteById(id);
+        blogRepository.flush();
+        return "redirect:/admin/blogs";
+    }
+
 }
--- src/main/java/repository/BlogRepository.java
+++ src/main/java/repository/BlogRepository.java
@@ -3,8 +3,21 @@ package repository;
 
 import model.BlogEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
-@Repository
+import java.util.Date;
+
 public interface BlogRepository extends JpaRepository<BlogEntity,Integer> {
+
+    // 修改博文操作
+    @Modifying
+    @Transactional
+    @Query("update BlogEntity set title =:pTitle,userByUserId.id = :pUserID" +
+            ",content=:pContent,pubDate=:pPubDate where id = :pID ")
+    void updateBlogs(@Param("pTitle") String title, @Param("pUserID") int userID, @Param("pContent") String content
+        , @Param("pPubDate") Date pubDate,@Param("pID") int id);
 }
